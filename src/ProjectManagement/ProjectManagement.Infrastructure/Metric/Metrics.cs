8c70392f3635befd7d57ff9ddb382642e4d9c6e2
Yuri Melo <yurifullstack@gmail.com>
2025-08-10 07:44:13 -0300

    Update data type from int to long

--- src/ProjectManagement/ProjectManagement.Infrastructure/Metric/Metrics.cs
+++ src/ProjectManagement/ProjectManagement.Infrastructure/Metric/Metrics.cs
@@ -7,6 +7,6 @@ public static class Metrics
     public static readonly Meter GreeterMeter = new Meter("OtPrGrYa.Example", "1.0.0");
     public static readonly Meter CreateOrganizationMeter = new Meter("OtPrCrOr.Example", "1.0.0");
 
-    public static readonly Counter<int> CountGreetings = GreeterMeter.CreateCounter<int>("greetings.count", description: "Counts the number of greetings");
-    public static readonly Counter<int> CountOrganizationsCreated = CreateOrganizationMeter.CreateCounter<int>("organizations.created.count", description: "Counts the number of organizations created");
+    public static readonly Counter<long> CountGreetings = GreeterMeter.CreateCounter<long>("greetings.count", description: "Counts the number of greetings");
+    public static readonly Counter<long> CountOrganizationsCreated = CreateOrganizationMeter.CreateCounter<long>("organizations.created.count", description: "Counts the number of organizations created");
 }
