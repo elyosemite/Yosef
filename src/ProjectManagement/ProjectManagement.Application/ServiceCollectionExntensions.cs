f679157113e18786ed7b96d835b55a73aeede6ab
Yuri Melo <yurifullstack@gmail.com>
2025-08-17 11:38:51 -0300

    Add mdiator for Application layer

---
+++ src/ProjectManagement/ProjectManagement.Application/ServiceCollectionExntensions.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+using Mediator;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProjectManagement.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssemblyContaining(typeof(ServiceCollectionExtensions));
+
+        services.AddMediator(
+            (MediatorOptions options) =>
+            {
+                options.Assemblies = [typeof(ServiceCollectionExtensions)];
+                options.ServiceLifetime = ServiceLifetime.Scoped;
+            }
+        );
+    }
+}
