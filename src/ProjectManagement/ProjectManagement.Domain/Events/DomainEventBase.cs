aa56121fc4c19285e4587db8ffaf94eeef072b33
Yuri Melo <yurifullstack@gmail.com>
2025-08-22 21:44:38 -0300

    Replace record to class

--- src/ProjectManagement/ProjectManagement.Domain/Events/DomainEventBase.cs
+++ src/ProjectManagement/ProjectManagement.Domain/Events/DomainEventBase.cs
@@ -1,6 +1,6 @@
 namespace Yosef.ProjectManagement.Domain.Events;
 
-public abstract record DomainEventBase
+public abstract class DomainEventBase
 {
     public DateTime OccurredOn { get; protected set; } = DateTime.UtcNow;
 }
