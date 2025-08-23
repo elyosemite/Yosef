c08db3c26b1321a7c8c7c084a23302bdc56ccd3f
Yuri Melo <yurifullstack@gmail.com>
2025-08-22 21:47:18 -0300

    Implement IDomainEventDispatcher

---
+++ src/ProjectManagement/ProjectManagement.Infrastructure/DispatcherHandler/DomainEventDipatcher.cs
@@ -0,0 +1,44 @@
+using Mediator;
+using Microsoft.Extensions.Logging;
+using ProjectManagement.Domain.Events;
+using ProjectManagement.Infrastructure.DispatcherHandler;
+using Yosef.ProjectManagement.Domain.Events;
+
+namespace Yosef.ProjectManagement.Infrastructure.DispatcherHandler;
+
+public class DomainEventDispatcher : IDomainEventDispatcher
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<DomainEventDispatcher> _logger;
+
+    public DomainEventDispatcher(IMediator mediator, ILogger<DomainEventDispatcher> logger)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task DispatchEventAsync(IEnumerable<IHasDomainEvent> domainEvents)
+    {
+        foreach (IHasDomainEvent entity in domainEvents)
+        {
+            if (entity is HasDomainEventBase hasDomainEvent)
+            {
+                DomainEventBase[] events = hasDomainEvent.DomainEvents.ToArray();
+                hasDomainEvent.ClearDomainEvents();
+
+                if (events.Any())
+                {
+                    var dispatcherNotification = new DispatcherNotification(events);
+                    await _mediator.Publish(dispatcherNotification);
+                }
+            }
+            else
+            {
+                _logger.LogError(
+                    "Entity of type {EntityType} does not inherit from {BaseType}. Unable to clear domain events.",
+                    entity.GetType().Name,
+                    nameof(HasDomainEventBase));
+            }
+        }
+    }
+}
