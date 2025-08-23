e04264370e761f7eaa4ac3aabcbb98a2bea3f14c
Yuri Melo <yurifullstack@gmail.com>
2025-08-22 21:45:19 -0300

    Change the namespace to DispatcherHandler

--- src/ProjectManagement/ProjectManagement.Infrastructure/EventProcessor/IDomainEventDispatcher.cs
+++
@@ -1,8 +0,0 @@
-using Yosef.ProjectManagement.Domain.Events;
-
-namespace Yosef.ProjectManagement.Infrastructure.EventProcessor;
-
-public interface IDomainEventDispatcher
-{
-    Task DispatchEventsAsync(IEnumerable<IHasDomainEvent> entities);
-}
