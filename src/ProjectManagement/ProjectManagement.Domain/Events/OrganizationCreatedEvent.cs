0d0292ce557b95902812900087ab301a43769596
Yuri Melo <yurifullstack@gmail.com>
2025-08-25 05:35:19 -0300

    Add public properties and fields

--- src/ProjectManagement/ProjectManagement.Domain/Events/OrganizationCreatedEvent.cs
+++ src/ProjectManagement/ProjectManagement.Domain/Events/OrganizationCreatedEvent.cs
@@ -2,11 +2,25 @@ namespace Yosef.ProjectManagement.Domain.Events;
 
 public class OrganizationCreatedEvent : DomainEventBase
 {
+    private readonly Guid _organizationId;
+    private readonly string _name;
+    private readonly string _description;
+    private readonly DateTime _createdAt;
+
+    public Guid OrganizationId => _organizationId;
+    public string Name => _name;
+    public string Description => _description;
+    public DateTime CreatedAt => _createdAt;
+
     public OrganizationCreatedEvent(
         Guid OrganizationId,
         string Name,
         string Description,
         DateTime CreatedAt)
     {
+        _organizationId = OrganizationId;
+        _name = Name;
+        _description = Description;
+        _createdAt = CreatedAt;
     }
 }
