3768ae6acde3bd98ca4cbbebd6f9d2a6863fa4bd
Yuri Melo <yurifullstack@gmail.com>
2025-08-25 05:34:44 -0300

    Add public properties and fields

--- src/ProjectManagement/ProjectManagement.Domain/Events/OrganizationArchievedEvent.cs
+++ src/ProjectManagement/ProjectManagement.Domain/Events/OrganizationArchievedEvent.cs
@@ -2,9 +2,21 @@ namespace Yosef.ProjectManagement.Domain.Events;
 
 public class OrganizationArchivedEvent : DomainEventBase
 {
+    private readonly Guid _organizationId;
+    private readonly DateTime _archivedAt;
+    private readonly bool _active;
+
     public OrganizationArchivedEvent(
         Guid OrganizationId,
-        DateTime ArchivedAt)
+        DateTime ArchivedAt,
+        bool active = false)
     {
+        _organizationId = OrganizationId;
+        _archivedAt = ArchivedAt;
+        _active = active;
     }
+
+    public Guid OrganizationId => _organizationId;
+    public DateTime ArchivedAt => _archivedAt;
+    public bool Active => _active;
 }
