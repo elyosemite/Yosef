107d3ebcda931167b85a749d5c197595bd3f1801
Yuri Melo <yurifullstack@gmail.com>
2025-08-30 18:44:06 -0300

    Add generic route

--- src/YosefFrontend/src/router/AppRouter.tsx
+++ src/YosefFrontend/src/router/AppRouter.tsx
@@ -1,11 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Users from "../pages/Users";
+import Page from "@/dashboard/page";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Page />} />
+
         <Route path="/users" element={<Users />} />
+
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
