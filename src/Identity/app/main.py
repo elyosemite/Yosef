41260f2e6b36ae545869875ca5295e0930c90da3
Yuri Melo <yurifullstack@gmail.com>
2025-09-08 20:47:26 -0300

    feat: configure in main

--- src/Identity/app/main.py
+++ src/Identity/app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from .routers import token
+from .routers import token, users
 from .config import settings
 
 app = FastAPI(title="Identity Service")
 
 app.include_router(token.router)
+app.include_router(users.router)
 
 @app.get("/")
 async def root():
