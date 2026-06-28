931ff7321597a0a72f9beae8473c79ba19a01ccb
Yuri Melo <yurifullstack@gmail.com>
2026-06-28 13:13:53 -0300

    delete old wb project

--- src/YosefFrontend/src/api/userApi.ts
+++
@@ -1,9 +0,0 @@
-import type { User } from "../types/user";
-
-const BASE_URL = "http://localhost:3000";
-
-export async function getUsers(): Promise<User[]> {
-  const res = await fetch(`${BASE_URL}/users`);
-  if (!res.ok) throw new Error("Error to get users");
-  return res.json();
-}
